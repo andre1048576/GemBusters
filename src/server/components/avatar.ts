2de3b147125522c260806ebe781e11581cc6d546
andre1048576 <55166043+andre1048576@users.noreply.github.com>
2025-08-26 10:19:18 -0400

    jump animation added

--- src/server/components/avatar.ts
+++ src/server/components/avatar.ts
@@ -6,6 +6,7 @@ import { PathTile } from "server/tiles/tile";
 import { TileConstructor } from "server/tiles/tile_constructor";
 import { MoveOptions, remotes } from "../../shared/remotes";
 import { Direction } from "shared/path";
+import BezierCurve from "@rbxts/bezier";
 
 export class Avatar extends TagComponent<AvatarClass> {
 	private animator = this.Object.FindFirstChild("Animator", true) as Animator;
@@ -13,6 +14,8 @@ export class Avatar extends TagComponent<AvatarClass> {
 	private walk_anim = this.animator.LoadAnimation(this.walk_track);
 	private climb_track = this.Object.FindFirstChild("Climb", true) as Animation;
 	private climb_anim = this.animator.LoadAnimation(this.climb_track);
+	private jump_track = this.Object.FindFirstChild("Jump", true) as Animation;
+	private jump_anim = this.animator.LoadAnimation(this.jump_track);
 
 	private move_model(model: AvatarClass, end_part: Part, current_part: Part | undefined) {
 		const SPEED = this.GridSize * 3;
@@ -50,30 +53,29 @@ export class Avatar extends TagComponent<AvatarClass> {
 	public GridSize!: number;
 	public tileOn?: PathTile;
 
-	public health: number = 7
-	public energy: number = 5
+	public health: number = 7;
+	public energy: number = 5;
 
-	public modify_health(delta : number) {
-		this.health = math.clamp(this.health+delta,0,7)
-		this.on_health_change()
+	public modify_health(delta: number) {
+		this.health = math.clamp(this.health + delta, 0, 7);
+		this.on_health_change();
 	}
 
-	public modify_energy(delta : number) {
-		this.energy = math.clamp(this.energy+delta,0,5)
-		this.on_energy_change()
+	public modify_energy(delta: number) {
+		this.energy = math.clamp(this.energy + delta, 0, 5);
+		this.on_energy_change();
 	}
 
 	public on_health_change!: () => void;
 	public on_energy_change!: () => void;
-	public create_path!: (dirs : Direction[]) => Part[]
+	public create_path!: (dirs: Direction[]) => Part[];
 	public attack_tile!: (pos: Vector3) => void;
 	public place_boulder!: (pos: Vector3) => void;
-	public get_tile!: (pos:Vector3) => PathTile;
+	public get_tile!: (pos: Vector3) => PathTile;
 
-	private Move(dirs : Direction[]) {
+	private Move(dirs: Direction[]) {
 		const path = this.create_path(dirs) as Part[];
-
-		this.Object.SetAttribute("Position", path[path.size()-1].GetAttribute("Position"));
+		this.Object.SetAttribute("Position", path[path.size() - 1].GetAttribute("Position"));
 		this.walk_anim.Play();
 		this.Trove.add(
 			task.spawn(() => {
@@ -90,19 +92,41 @@ export class Avatar extends TagComponent<AvatarClass> {
 				});
 				this.walk_anim.Stop();
 				this.tileOn!.landed(this);
-				remotes.avatar_selected.fire(this.player, false,undefined);
+				remotes.avatar_selected.fire(this.player, false, undefined);
 				this.Object.MoveFinished.Fire();
 			}),
 		);
 	}
 
-	private Jump(goal : Vector3) {
-		this.Object.SetAttribute("Position",goal);
-		this.tileOn?.left(this)
+	private Jump(goal: Vector3) {
+		const model = this.Object;
+		this.Object.SetAttribute("Position", goal);
+		this.tileOn?.left(this);
 		this.tileOn?.steppedOut(this);
 		const goal_tile = this.get_tile(goal);
 		this.tileOn = goal_tile;
-		this.Object.PivotTo(goal_tile.Object.GetPivot());
+
+		this.jump_anim.Play(0);
+
+		const SPEED = this.GridSize * 3;
+		const initial_point = model.GetPivot().Position;
+		const end_point = goal_tile.Object.GetPivot().Position;
+		const end_no_y = new Vector3(end_point.X, initial_point.Y, end_point.Z);
+		const delta = end_no_y.sub(initial_point);
+		const distance = delta.Magnitude;
+		//this.Object.PivotTo(goal_tile.Object.GetPivot());
+
+		const curve = new BezierCurve([
+			initial_point,
+			initial_point.Lerp(end_no_y, 0.07).add(new Vector3(0, (this.GridSize * 8) / 5, 0)),
+			initial_point.Lerp(end_no_y, 0.93).add(new Vector3(0, (this.GridSize * 8) / 5, 0)),
+			end_point,
+		]);
+
+		Tween(distance / SPEED, Linear, (percent_travelled) => {
+			model.PivotTo(CFrame.lookAlong(curve.calculate(percent_travelled), delta));
+		}).Wait();
+		this.jump_anim.Stop();
 		goal_tile.steppedIn(this);
 		goal_tile.landed(this);
 		this.Object.MoveFinished.Fire();
@@ -122,17 +146,17 @@ export class Avatar extends TagComponent<AvatarClass> {
 	}
 
 	public selected() {
-		remotes.avatar_selected.fire(this.player, true,this.Object);
+		remotes.avatar_selected.fire(this.player, true, this.Object);
 		let goal: any;
 		let action: MoveOptions;
 		this.Trove.addPromise(
 			remotes.avatar_option_selected
 				.promise(
-					(p,_1,_?) => p === this.player,
-					(_,m_type,_goal?) => {
-						goal = _goal
+					(p, _1, _?) => p === this.player,
+					(_, m_type, _goal?) => {
+						goal = _goal;
 						action = m_type;
-					}
+					},
 				)
 				.andThenCall(() => {
 					switch (action) {
